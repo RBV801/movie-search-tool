b00b028b9c60ba20526fc0807459c9efd65e1692
RBV801 <rakmunbv@gmail.com>
2024-12-02 17:03:50 -0800

    Update movie-search-ui.js
    manual changes

--- public/movie-search-ui.js
+++ public/movie-search-ui.js
@@ -3,16 +3,39 @@ Vue.component('movie-search-ui', {
     <div>
       <input type="text" v-model="searchQuery" placeholder="Enter movie title" />
       <button @click="search">Search</button>
+      <div v-if="isLoading">Loading...</div>
+      <div v-if="error" class="error">{{ error }}</div>
+      <div v-if="movies.length">
+        <h3>Search Results:</h3>
+        <ul>
+          <li v-for="movie in movies" :key="movie.id">
+            {{ movie.title }} ({{ movie.year }})
+          </li>
+        </ul>
+      </div>
     </div>
   `,
   data() {
     return {
-      searchQuery: ''
+      searchQuery: '',
+      movies: [],
+      isLoading: false,
+      error: null,
+      movieScraper: new MovieScraper()
     };
   },
   methods: {
-    search() {
-      this.$emit('search', this.searchQuery);
+    async search() {
+      try {
+        this.isLoading = true;
+        this.error = null;
+        this.movies = await this.movieScraper.searchMovie(this.searchQuery);
+        this.$emit('search-complete', this.movies);
+      } catch (error) {
+        this.error = error.message;
+      } finally {
+        this.isLoading = false;
+      }
     }
   }
 });
